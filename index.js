5627788c654b3d76a2f627dd2e79d3292176818d
Iremide-ds <iremideadeboye@gmail.com>
2023-03-04 21:05:02 +0100

    day three

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const factorial_1 = __importDefault(require("./day_1/factorial"));
 const palindrome_1 = __importDefault(require("./day_2/palindrome"));
+const largest_in_array_1 = __importDefault(require("./day_3/largest_in_array"));
 //day one
 console.log((0, factorial_1.default)(10));
 //day two
 const randomStr = "met";
 console.log(`${randomStr} is a palindrome? ` + (0, palindrome_1.default)(randomStr));
+//day three
+const randArr = [1, 2, 3, 4, 5, 9, 7, 6];
+console.log((0, largest_in_array_1.default)(randArr));
--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import findFactorial from "./day_1/factorial";
 import checkPalindrome from "./day_2/palindrome";
+import findHighestNum from "./day_3/largest_in_array";
 
 //day one
 console.log(findFactorial(10));
@@ -7,3 +8,7 @@ console.log(findFactorial(10));
 //day two
 const randomStr = "met";
 console.log(`${randomStr} is a palindrome? ` + checkPalindrome(randomStr));
+
+//day three
+const randArr = [1, 2, 3, 4, 5, 9, 7, 6];
+console.log(findHighestNum(randArr));
